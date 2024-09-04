b634d1af36035c7b7b18990d0c909e9ec8fd32eb
Fatimah Alzahrani <12fatimah.15@gmail.com>
2024-09-04 10:29:25 +0300

    States with Form

--- src/Form.js
+++ src/Form.js
@@ -9,7 +9,14 @@ let formStyle = {
   backgroundColor: "lightgrey",
 };
 export default function Form() {
-  const [inputs, setInputs] = useState({ name: "", email: "" });
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    gender: "",
+    city: "",
+    comment: "",
+    agree: false,
+  });
   return (
     <form
       style={formStyle}
@@ -37,6 +44,61 @@ export default function Form() {
         value={inputs.email}
         onChange={(event) => {
           setInputs({ ...inputs, email: event.target.value });
+        }}
+      />
+      <hr />
+      <hr />
+      <label>الجنس : </label>
+      <div>
+        <input
+          type="radio"
+          value="ذكر"
+          checked={inputs.gender === "ذكر"}
+          onChange={(event) => {
+            setInputs({ ...inputs, gender: event.target.value });
+          }}
+        />
+        ذكر
+        <input
+          type="radio"
+          value="أنثى"
+          checked={inputs.gender === "أنثى"}
+          onChange={(event) => {
+            setInputs({ ...inputs, gender: event.target.value });
+          }}
+        />
+        أنثى
+      </div>
+      <hr />
+      <label>اختر مدينتك : </label>
+      <select
+        value={inputs.city}
+        onChange={(event) => {
+          setInputs({ ...inputs, city: event.target.value });
+        }}
+      >
+        <option>الباحة</option>
+        <option>الطايف</option>
+        <option>المدينة</option>
+        <option>مكة</option>
+      </select>
+      <hr />
+      <label>لديك اي ملاحظات؟ </label>
+      <textarea
+        value={inputs.comment}
+        onChange={(event) => {
+          setInputs({ ...inputs, comment: event.target.value });
+        }}
+      >
+        {inputs.comment}
+      </textarea>
+      <hr />
+      <label>موافقة على الشروط والأحكام</label>
+      <input
+        type="checkbox"
+        checked={inputs.agree}
+        onChange={(event) => {
+          setInputs({ ...inputs, agree: event.target.checked });
         }}
       />
       <hr />
